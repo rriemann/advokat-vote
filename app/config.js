63674c8d08330d6abd28c4bca53c06c2165c8ca8
Robert Riemann <robert@riemann.cc>
2017-08-27 14:38:59 +0200

    update dev npms

--- app/config.js
+++ app/config.js
@@ -17,7 +17,7 @@ System.config({
     "angular-messages": "github:angular/bower-angular-messages@1.5.8",
     "angular-mocks": "github:angular/bower-angular-mocks@1.5.8",
     "css": "github:systemjs/plugin-css@0.1.21",
-    "json": "github:systemjs/plugin-json@0.1.0",
+    "json": "github:systemjs/plugin-json@0.1.2",
     "text": "github:systemjs/plugin-text@0.0.4",
     "traceur": "github:jmcriffey/bower-traceur@0.0.93",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93",
